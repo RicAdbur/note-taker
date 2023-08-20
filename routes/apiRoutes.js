72dccf2c0ec44b4dfda52982c098f2c319332ae9
Max <maxrrice@hotmail.com>
2023-08-20 17:17:36 -0500

    delete route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,6 +4,7 @@ const fs = require("fs/promises")
 const dbPath = path.join(__dirname, "..", "db", "db.json")
 const nanoid = require("nanoid")
 
+// GET route
 router.get("/api/notes", async (req, res) => {
   try {
     const parsedDbJson = JSON.parse(await fs.readFile(dbPath))
@@ -14,6 +15,7 @@ router.get("/api/notes", async (req, res) => {
   }
 });
 
+// POST route
 router.post("/api/notes", async (req, res) => {
   try {
     const notes = JSON.parse(await fs.readFile(dbPath, "utf-8"))
@@ -30,10 +32,23 @@ router.post("/api/notes", async (req, res) => {
   }
 })
 
-
-
-
-
-
+// DELETE route
+router.delete("/api/notes/:id", async (req, res) => {
+  try{
+    const notes = JSON.parse(await fs.readFile(dbPath, "utf-8"))
+    const noteIndex = notes.findIndex((note) => note.id === req.params.id)
+    if (noteIndex === -1) {
+      res.status(404).json("No note found")
+      return
+    } else {
+      notes.splice(noteIndex)
+    }
+    await fs.writeFile(dbPath, JSON.stringify(notes))
+    res.json(notes)
+  } catch(err) {
+    res.status(500).send(err)
+    console.error(err)
+  }
+})
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,16 +1,18 @@
+// imports
 const express = require("express");
 const pageRoutes = require("./routes/pageRoutes");
 const apiRoutes = require("./routes/apiRoutes");
 const PORT = 3000;
 const app = express();
 
+// middleware
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(pageRoutes);
 app.use(apiRoutes);
 
-
+// listener
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`)
 });
